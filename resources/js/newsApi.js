4a68bb18a0b583c0f5f882e3e7ae4bb7eb967145
Schnee <choe@Carstens-MBP.fritz.box>
2022-04-29 17:28:32 +0200

    [master] removed unused variable

--- resources/js/newsApi.js
+++ resources/js/newsApi.js
@@ -1,24 +1,36 @@
 /* variables */
-const headerToken = "X-Heise-Token: zcJulkgE";
-const baseUrlNews= "https://www.heise.de/extras/frontend/news/";
+const baseUrlNews= "https://www.heise.de/extras/frontend/news/?limit=5";
 const loadNewsBtn = document.getElementById('load-news-btn');
 
-/* const inputField = document.getElementById('input-field');
-let city = document.getElementById("city");
-let temp = document.getElementById("temp");
-let condition = document.getElementById("condition");
-let fetched = false; */ 
 
 
 /* function definition */
 const loadNews = () => {
+    /* within root */
+    let id = null;
+    let title = [];
+    let synopsis = null;
 
+    /* within meta */
+    let dateDate = null; 
+    let dateTime = null; 
+    let imgSrc = null
+
+    /* within meta but only accessible with news-id */
+    let content = null
+    
     fetch(baseUrlNews, {
         headers: {"X-Heise-Token": "zcJulkgE"},
     })
         .then((response) => (response.json()))
         .then((data) => {
             console.log('Daten: ' + JSON.stringify(data));
+
+            title = data[0].title
+
+
+            
+            console.log('Der Titel lautet: ' + title);
         })
 }
 
