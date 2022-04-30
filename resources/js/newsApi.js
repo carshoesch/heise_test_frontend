7d772a81797454bae5ff4cbcbd239b0cac5f8ed9
Schnee <choe@Carstens-MBP.fritz.box>
2022-04-30 19:47:56 +0200

    [master] added api output within news-wrapper

--- resources/js/newsApi.js
+++ resources/js/newsApi.js
@@ -1,6 +1,9 @@
 /* variables */
-const baseUrlNews= "https://www.heise.de/extras/frontend/news/?limit=5";
-const loadNewsBtn = document.getElementById('load-news-btn');
+const baseUrlNews = "https://www.heise.de/extras/frontend/news/"
+const baseUrlNewsLimit= "https://www.heise.de/extras/frontend/news/?limit=";
+const limiter = 10
+const loadNewsBtn = document.getElementById('loadNewsBtn');
+const newsWrapper = document.getElementById('news');
 const newsTileWrapper = document.getElementById('newsTileWrapper');
 let newsArray = [];
 
@@ -8,37 +11,22 @@ let newsArray = [];
 
 /* function definition */
 const loadNews = () => {
-    /* within root */
-    let id = null;
-    let title = [];
-    let synopsis = null;
-
-    /* within meta */
-    let dateDate = null; 
-    let dateTime = null; 
-    let imgSrc = null;
-
-    /* within meta but only accessible with news-id */
-    let content = null
     
-    fetch(baseUrlNews, {
+    fetch(baseUrlNewsLimit 
+        + limiter, {
         headers: {"X-Heise-Token": "zcJulkgE"},
     })
         .then((response) => (response.json()))
         .then((data) => {
             newsArray = data;
             
-            /* console.log('Das Array lautet: ' + newsArray); */
-            for (let index = 0; index < newsArray.length; index++) {
-                
-            }
             displayNews(newsArray);
         })
 
 }
 
 const displayNews = (newsArray) => {
-    /* newsTileWrapper.innerHTML = ""; */
+    console.log('Data: ' + newsArray);
 
     if (newsArray.length == 0) {
         newsTileWrapper.innerHTML = '<h5>Keine News vorhanden</h5>'
@@ -46,11 +34,50 @@ const displayNews = (newsArray) => {
     } else {
         /* TODO: create HTML-Blocks for every news-element within the array */
         newsArray.forEach(news => {
-            /* const div = document.createElement('div');
-            div.className = "news-tile-wrapper";
-            const overview = document.createElement('div');
-            overview.className = "news__overview--title";
-            div.appendChild(overview); */
+
+            console.log(JSON.stringify(news));
+
+            const title = news.title;
+            const fullText = news.synopsis;
+            const dateTime = news.meta.pubDate;
+            const date = dateTime.split('T')[0];
+            const time = dateTime.split('T')[1].split('+')[0];
+            const id = news.id;
+
+            const newNews = document.createElement('div');
+            newNews.classList.add('news-tile-wrapper');
+            newsWrapper.appendChild(newNews);
+
+            const pubDateTime = document.createElement('div');
+            pubDateTime.classList.add('news__dateTime');
+            newNews.appendChild(pubDateTime);
+
+            const pubDate = document.createElement('div');
+            pubDate.classList.add('news__dateTime--date');
+            pubDate.innerHTML = date;
+            pubDateTime.appendChild(pubDate);
+
+            const pubTime = document.createElement('div');
+            pubTime.classList.add('news__dateTime--time');
+            pubTime.innerHTML = time;
+            pubDateTime.appendChild(pubTime);
+
+            const heading = document.createElement('div');
+            heading.classList.add('news__overview--title');
+            heading.innerHTML = title;
+            newNews.appendChild(heading);
+
+           /*  const teaser = document.createElement('div');
+            teaser.classList.add('news__overview--teaser');
+            teaser.innerHTML = teaserText;
+            newNews.appendChild(teaser); */
+
+            const detail = document.createElement('a');
+            const linkText = document.createTextNode("Mehr erfahren");
+            detail.appendChild(linkText);
+            detail.href = baseUrlNews+id;
+            detail.target="_blank";
+            newNews.appendChild(detail)
         })
     }
 
